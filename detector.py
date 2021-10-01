0610fe85a069e97f19fc6e5cb697012eb4288349
LoneHandyman <78569671+LoneHandyman@users.noreply.github.com>
2021-10-01 12:05:29 -0500

    optimized detection

--- detector.py
+++ detector.py
@@ -9,11 +9,12 @@ class FaceCamDetector:
     if not (self.camera_device.isOpened()):
       print("Error: Could not open your camera device.")
       exit(1)
-    self.classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    self.classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt.xml")
 
   def _detect_face(self, frame):
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    detected_faces = self.classifier.detectMultiScale(gray_frame, scaleFactor=1.1,
+    blur_frame = cv2.GaussianBlur(gray_frame, (45, 45), cv2.BORDER_DEFAULT)
+    detected_faces = self.classifier.detectMultiScale(blur_frame, scaleFactor=1.1,
                                 minNeighbors=3, minSize=(30,30), maxSize=(200,200))
     main_face = None
     max_area = 0
@@ -23,9 +24,9 @@ class FaceCamDetector:
         max_area = new_max_area
         main_face = (x, y, w, h)
       if(self.display_image_op):
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
+        cv2.rectangle(blur_frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
     if(self.display_image_op):
-      cv2.imshow('FaceCamDetector', frame)
+      cv2.imshow('FaceCamDetector', blur_frame)
     return main_face
 
   def update_frame_by_frame(self):
