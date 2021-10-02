e5a7eab6f72d29dcdee25a82e35398d98281a952
LoneHandyman <78569671+LoneHandyman@users.noreply.github.com>
2021-10-01 22:06:48 -0500

    Wrapper

---
+++ ConnectionManager.py
@@ -0,0 +1,63 @@
+import copy
+import cv2
+import math
+import numpy
+import socket
+import OpenCvObjDetectors as trackers
+
+sigmoid = lambda x: 1 / (1 + numpy.exp(-x))
+FLOAT_PRECISION = 4
+
+class ConnectionManager():
+  def __init__(self, unity_server_ip, unity_server_port, draw_enabled=False):
+    self.FACE_CLASSIFIER_NAME = "haarcascade_frontalface_alt.xml"
+    self.camera_device = cv2.VideoCapture(0)
+    if not (self.camera_device.isOpened()):
+      print("<Error>: Could not open your camera device.")
+      exit(1)
+    self.camera_device.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    self.camera_device.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    classifier_path = cv2.data.haarcascades + self.FACE_CLASSIFIER_NAME
+    self.face_classifier = cv2.CascadeClassifier(classifier_path)
+    print("<Source>:", self.face_classifier)
+    print("<Path>:", classifier_path)
+
+    self.draw_enabled = draw_enabled
+    self.unity_server_ip = unity_server_ip
+    self.unity_server_port = unity_server_port
+    self.udp_connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+  def run(self):
+    face_detector = trackers.FaceDetector(classifier=self.face_classifier, display_enabled=self.draw_enabled)
+    hand_detector = trackers.HandDetector(detection_confidence=0.7, display_enabled=self.draw_enabled)
+    last_x_factor_value = 0.0
+    while(True):
+      retval, frame = self.camera_device.read()
+      if(retval):
+        copy_frame = copy.copy(frame)
+        box, mod_face_frame = face_detector.find_face_box(frame)
+        landmarks, mod_hand_frame = hand_detector.find_hand_graph(copy_frame)
+
+        norm_movement_fact = norm_acceleration_fact = 0.0
+        if box is not None:
+          x_vel = float(box[0]) + float(box[2]) * 0.5
+          norm_movement_fact = round(sigmoid(x_vel - last_x_factor_value) * 2 - 1, FLOAT_PRECISION)
+          last_x_factor_value = x_vel
+          print("<Movement_Factor>: %7.4f" % (norm_movement_fact))
+
+        if len(landmarks) != 0:
+          x1, y1 = landmarks[4][1], landmarks[4][2]
+          x2, y2 = landmarks[8][1], landmarks[8][2]
+          norm_acceleration_fact = round(min(math.hypot(x2 - x1, y2 - y1), 160.0)/160.0, FLOAT_PRECISION)
+          print("<Acceleration Factor>: %7.4f" % (norm_acceleration_fact))
+        #self.udp_connection.sendto(str(norm_movement_fact) + " " + str(norm_acceleration_fact), (self.unity_server_ip, self.unity_server_port))
+        if(self.draw_enabled):
+          cv2.imshow("Face", mod_face_frame)
+          cv2.imshow("Hand", mod_hand_frame)
+
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+if __name__ == '__main__':
+  demo = ConnectionManager("127.0.0.1", 50001, True)
+  demo.run()
---
+++ OpenCvObjDetectors.py
@@ -0,0 +1,57 @@
+import cv2
+import mediapipe as mp
+
+class FaceDetector():
+  def __init__(self, classifier, display_enabled=False):
+    self.classifier = classifier
+    self.display_image_op = display_enabled
+
+  def _detect(self, frame):
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur_frame = cv2.GaussianBlur(gray_frame, (45, 45), cv2.BORDER_DEFAULT)
+    detected_targets = self.classifier.detectMultiScale(blur_frame, scaleFactor=1.1,
+                                  minNeighbors=3, minSize=(30,30), maxSize=(200,200))
+    return detected_targets, blur_frame
+
+  def find_face_box(self, frame):
+    detected_faces, mod_frame = self._detect(frame)
+    main_face = None
+    max_area = 0
+    for (x, y, w, h) in detected_faces:
+      new_max_area = w * h * 0.5
+      if(new_max_area > max_area):
+        max_area = new_max_area
+        main_face = (x, y, w, h)
+      if(self.display_image_op):
+        cv2.rectangle(mod_frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
+    return main_face, mod_frame
+
+class HandDetector():
+  def __init__(self, mode=False, max_hands=2, detection_confidence=0.5, track_confidence=0.5, display_enabled=False):
+    self.mp_hands = mp.solutions.hands
+    self.hands = self.mp_hands.Hands(mode, max_hands, detection_confidence, track_confidence)
+    self.mp_draw = mp.solutions.drawing_utils
+    self.display_image_op = display_enabled
+
+  def _find_hands(self, frame):
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    self.results = self.hands.process(frame_rgb)
+    if self.results.multi_hand_landmarks:
+        for hand_lms in self.results.multi_hand_landmarks:
+            if self.display_image_op:
+                self.mp_draw.draw_landmarks(frame, hand_lms,
+                                            self.mp_hands.HAND_CONNECTIONS)
+    return frame
+
+  def find_hand_graph(self, frame, hand_idx=0):
+    mod_frame = self._find_hands(frame)
+    landmark_list = []
+    if self.results.multi_hand_landmarks:
+        selected_hand = self.results.multi_hand_landmarks[hand_idx]
+        for id, lm in enumerate(selected_hand.landmark):
+            h, w, c = mod_frame.shape
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            landmark_list.append([id, cx, cy])
+            if self.display_image_op:
+                cv2.circle(mod_frame, (cx, cy), 4, (0, 255, 0), cv2.FILLED)
+    return landmark_list, mod_frame
